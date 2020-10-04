db6ea180d4d09025d9d858c41026d8b0bf54f064
yangyang0028 <71932972+yangyang0028@users.noreply.github.com>
2020-10-04 21:18:06 +0800

    yangyang0028

--- B2.py
+++ B2.py
@@ -2,8 +2,7 @@ import sensor, image, time
 from pyb import UART
 import json
 
-
-class PID:
+class PID: #增量式PID
     def __init__(self, limit, target, feedback, Kp, Ki, Kd, e_0, e_1, e_2):
         self.limit=limit
         self.target=target
@@ -44,41 +43,38 @@ pid_y = PID(100, 0, 0,
 sensor.reset() # 初始化摄像头
 sensor.set_pixformat(sensor.RGB565) # 格式为 RGB565.
 sensor.set_framesize(sensor.QQVGA) # 使用 QQVGA 速度快一些
-sensor.set_auto_exposure(False,35000)
+sensor.set_auto_exposure(False,35000) # 设置曝光时间
 sensor.skip_frames(time = 2000) # 跳过2000s，使新设置生效,并自动调节白平衡
-sensor.set_auto_gain(False)
+sensor.set_auto_gain(False)# 关闭自动增益
 sensor.set_auto_whitebal(False)#关闭白平衡。白平衡是默认开启的，在颜色识别中，一定要关闭白平衡。
-clock = time.clock() # 追踪帧率
 uart = UART(3, 115200)
 
-control_val_x=1350
-control_val_y=1100
+control_val_x=1350#云台初始x位置
+control_val_y=1100#云台初始y位置
 
-MIN_LIMIT_y=950
-MAX_LIMIT_y=1250
+MIN_LIMIT_x=1000#云台x最小值
+MAX_LIMIT_x=1700#云台x最大值
 
-MIN_LIMIT_x=1000
-MAX_LIMIT_x=1700
+MIN_LIMIT_y=950#云台y最小值
+MAX_LIMIT_y=1250#云台y最大值
 
-ans_x=270
-ans_y=219
+ans_x=70#弹道落点在图片上的x坐标
+ans_y=54#弹道落点在图片上的y坐标
 
-aaa=28
+aaa=28#串口标志位
 
 while(True):
 
-    quan=0;
-    quan_id=-1
+    quan=0;#选取阈值后最大的目标区域
+    quan_id=-1#选取阈值后最大的目标区域ID
 
     img = sensor.snapshot(1.8) # 从感光芯片获得一张图像
-    img.draw_rectangle([70,54,10,10])
+    img.draw_rectangle([ans_x,ans_y,10,10])
     blobs = img.find_blobs([(32, 77, 23, 54, 2, 29)],pixels_threshold = 24,area_threshold = 5,merge = True)
     for b in blobs:
             if quan<b.pixels():
                 quan=b.pixels()
                 quan_id=b
-
-
     if quan > 0:
             x = quan_id[0]
             y = quan_id[1]
@@ -90,8 +86,8 @@ while(True):
             pid_x.feedback=x
             pid_y.feedback=y
 
-            pid_x.target=70
-            pid_y.target=54
+            pid_x.target=ans_x
+            pid_y.target=ans_y
 
             control_val_x += int(pid_x.pid_calc())
             control_val_y += int(pid_y.pid_calc())
@@ -101,12 +97,3 @@ while(True):
 
             print(control_val_x,control_val_y)
             uart.write(aaa.to_bytes(1,'int')+control_val_y.to_bytes(2,'int')+control_val_x.to_bytes(2,'int'))
-
-'''
-    for i in range(10):
-        if control_val_y>1250 :
-            control_val_y=950
-        control_val_y+=1
-        print(control_val_y)
-        uart.write(aaa.to_bytes(1,'int')+control_val_y.to_bytes(2,'int')+control_val_x.to_bytes(2,'int'))
-'''
